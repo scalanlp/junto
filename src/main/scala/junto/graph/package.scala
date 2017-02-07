e609558110982fc5b09a5326ca8eb4677bfb342c
Delip Rao <delip@joostware.com>
2017-02-06 16:52:24 -0800

    Fix the Floating point exception by skipping over the header

--- src/main/scala/junto/graph/package.scala
+++ src/main/scala/junto/graph/package.scala
@@ -40,8 +40,11 @@ package object graph {
   }
 
   def getLabels(inputFile: String, separator: Char = ','): Iterator[LabelSpec] = {
+    val lines = getSource(inputFile).getLines
+    val header = lines.next()
+    // skip the header for now (perhaps use it later to identify labels with probabilities)
     for {
-      line <- getSource(inputFile).getLines
+      line <- lines
       items = line.split(separator)
     } yield {
       val source = items(0)
